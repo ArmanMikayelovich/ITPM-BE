1f076d7fcf2acee50e2f81459d82b5aa21cad982
Arman Arshakyan <armanar@energizeglobal.com>
2020-07-13 19:56:55 +0400

    develop.

--- src/main/java/com/energizeglobal/itpm/Application.java
+++ src/main/java/com/energizeglobal/itpm/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//Bootstrap class
 public class Application {
 
     public static void main(String[] args) {
