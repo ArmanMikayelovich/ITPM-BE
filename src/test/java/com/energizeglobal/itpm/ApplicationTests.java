b4b9a8984a73ecdceadf4be94d56c186914a1da7
Arman Arshakyan <armanar@energizeglobal.com>
2020-07-13 18:19:12 +0400

    Initial commit.

---
+++ src/test/java/com/energizeglobal/itpm/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energizeglobal.itpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
