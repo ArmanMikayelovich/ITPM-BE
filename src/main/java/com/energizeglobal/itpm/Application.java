7a11d8bc72102935ae5f67ebed79a36ada290390
Arman Arshakyan <armanar@energizeglobal.com>
2020-07-21 15:40:48 +0400

    swagger addded. Seciruty disabled/

--- src/main/java/com/energizeglobal/itpm/Application.java
+++ src/main/java/com/energizeglobal/itpm/Application.java
@@ -2,9 +2,9 @@ package com.energizeglobal.itpm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
-//Bootstrap class
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Application {
 
 
